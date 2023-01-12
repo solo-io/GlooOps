de41346b22042a6a7cc7b9fe053e643035ef890c
Jona Apelbaum <jona.apelbaum@gmail.com>
2023-01-12 01:21:08 +0100

    change providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,9 +1,9 @@
 terraform {
   cloud {
-    organization = "jonaapelbaum"
+    organization = ""
 
     workspaces {
-      name = "argo-cd-demo"
+      name = ""
     }
   }
   required_providers {
